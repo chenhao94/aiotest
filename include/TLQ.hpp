5903430dd7c6f04c8d84c30bbe84cc570d923f88
Tongliang Liao <xkszltl@gmail.com>
2016-12-28 23:01:32 +0800

    Merge TLQ

--- include/TLQ.hpp
+++ include/TLQ.hpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <atomic>
+#include <thread>
 #include <functional>
 #include <vector>
 #include <chrono>
@@ -39,25 +40,80 @@ namespace tai
 
     public:
         // Check if there's anything to do.
-        size_t busy()
+        auto busy()
         {
             return todo.load(std::memory_order_relaxed);
         }
 
         // Switch wait/ready queue and pull at most one pending task.
-        void roll();
+        void roll()
+        {
+            ready.swap(wait);
+            Task* task = nullptr;
+            if (pending.pop(task))
+            {
+                ready.emplace_back(*task);
+                delete task;
+                task = nullptr;
+            }
+        }
 
-        // Setup next phase.
-        void setup(std::vector<Task>& queue);
-        void setupReady();
-        void setupDone();
+        // Clear and setup current queue for next phase.
+        void setup(std::vector<Task>& queue)
+        {
+            if (current)
+            {
+                current->clear();
+                if (!(rand() & 255))
+                    current->shrink_to_fit();
+            }
+            remain.store((current = &queue)->size(), std::memory_order_relaxed);
+        }
+
+        // Setup "Ready" phase.
+        void setupReady()
+        {
+            todo.fetch_add(ready.size(), std::memory_order_relaxed);
+            setup(ready);
+        }
+
+        // Setup "Done" phase.
+        void setupDone()
+        {
+            setup(done);
+        }
+
+        // Clear todo counter;
+        void clearTodo()
+        {
+            todo.store(0, std::memory_order_relaxed);
+        }
 
         // Pop and execute one task from the current queue.
-        size_t operator ()();
+        auto operator ()()
+        {
+            auto i = remain.fetch_sub(1, std::memory_order_relaxed);
+            if (i)
+                (*current)[i - 1]();
+            return i;
+        }
 
-        // Push a task into the queue specified.
-        void pushWait(const Task& task);
-        void pushDone(const Task& task);
-        void pushPending(const Task& task);
+        // Push a task into "wait" queue.
+        void pushWait(const Task& task)
+        {
+            wait.emplace_back(task);
+        }
+
+        // Push a task into "done" queue.
+        void pushDone(const Task& task)
+        {
+            done.emplace_back(task);
+        }
+
+        // Push a task into "pending" queue.
+        void pushPending(const Task& task)
+        {
+            pending.push(new Task(task));
+        }
     };
 }
