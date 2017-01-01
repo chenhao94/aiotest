3a5a7292f47415911d4bc375e3081fb7f989d533
Tongliang Liao <xkszltl@gmail.com>
2017-01-01 23:40:56 +0800

    Sync I/O test

--- src/main.cpp
+++ src/main.cpp
@@ -20,38 +20,60 @@ int main()
 
     using namespace tai;
 
-    default_random_engine rand;
-
-    Log::log("Creating B-tree...");
-    BTreeDefault bt("tmp");
-    // BTreeTrivial bt("tmp");
-
-    Log::log("Creating Controller...");
-    Controller ctrl(1 << 28, 1 << 30);
-
-    Log::log("Writing...");
-
     const auto size = (size_t)1 << 30;
     const auto bs = (size_t)1 << 12;
     const auto n = (size_t)1 << 20;
 
+    default_random_engine rand;
     uniform_int_distribution<size_t> dist(0, size - bs);
 
-    const auto start = high_resolution_clock::now();
     string data(bs, '0');
     for (auto& i : data)
         i += rand();
 
-    for (auto i = n; i--; bt.write(ctrl, dist(rand) % size, data.size(), data.data()));
-
     {
-        unique_ptr<IOCtrl> io(bt.sync(ctrl));
-        for (; (*io)() == IOCtrl::Running; sleep_for(seconds(1)))
-            Log::log("Still running...");
-        Log::log(to_cstring((*io)()));
+        Log::log("Creating file for sync I/O...");
+
+        fstream file("tmp-sync", ios_base::in | ios_base::out | ios_base::trunc | ios_base::binary);
+
+        Log::log("Writing...");
+
+        const auto start = high_resolution_clock::now();
+
+        for (auto i = n; i--; file.seekp(dist(rand) % size).write(data.data(), data.size()).flush())
+            if (i % (n / 100) == 0)
+                Log::log("\t", (n - i - 1) * 100 / n, "% Performance: ", (size_t)round((n - i - 1) * 1e9 / duration_cast<nanoseconds>(high_resolution_clock::now() - start).count()), " iops.");
+
+        Log::log("Performance: ", (size_t)round(n * 1e9 / duration_cast<nanoseconds>(high_resolution_clock::now() - start).count()), " iops.");
     }
 
-    Log::log("Performance: ", (size_t)round(n * 1e9 / duration_cast<nanoseconds>(high_resolution_clock::now() - start).count()), " iops.");
+    {
+        Log::log("Creating B-tree...");
+        // BTreeDefault bt("tmp");
+        BTreeTrivial bt("tmp-tai");
+
+        Log::log("Creating Controller...");
+        // Controller ctrl(1 << 28, 1 << 30, 1);
+        Controller ctrl(1 << 28, 1 << 30);
+
+        Log::log("Writing...");
+
+        const auto start = high_resolution_clock::now();
+
+        for (auto i = n; i--; bt.write(ctrl, dist(rand) % size, data.size(), data.data()))
+            if (i % (n / 100) == 0)
+                Log::log("\t", (n - i - 1) * 100 / n, "% Performance: ", (size_t)round((n - i - 1) * 1e9 / duration_cast<nanoseconds>(high_resolution_clock::now() - start).count()), " iops.");
+
+        {
+            unique_ptr<IOCtrl> io(bt.sync(ctrl));
+            for (; (*io)() == IOCtrl::Running; sleep_for(seconds(1)))
+                Log::log("Still running...");
+            Log::log(to_cstring((*io)()));
+        }
+
+        Log::log("Performance: ", (size_t)round(n * 1e9 / duration_cast<nanoseconds>(high_resolution_clock::now() - start).count()), " iops.");
+    }
+
     Log::log("Complete!");
     return 0;
 }
