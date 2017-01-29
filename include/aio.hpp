89764c2a90ba1a63e88fa0f537e06b8547619456
Tongliang Liao <xkszltl@gmail.com>
2017-01-29 09:55:30 -0600

    Fix

--- include/aio.hpp
+++ include/aio.hpp
@@ -4,47 +4,70 @@
  * POSIX AIO interace wrapper
  */
 
+#include <array>
 #include <memory>
 #include <atomic>
+
 #include <signal.h>
+
 #include "tai.hpp"
 
 namespace tai
 {
+    class aiocb;
+
+    void aio_init();
+    int aio_read(aiocb* aiocbp);
+    int aio_write(aiocb* aiocbp);
+    int aio_fsync(int op, aiocb* aiocbp);
+    int aio_error(aiocb* aiocbp);
+    int aio_return(aiocb* aiocbp);
+    bool register_fd(int fd);
+    void deregister_fd(int fd);
+
     class aiocb
     {
+        friend void aio_init();
+        friend int aio_read(aiocb*);
+        friend int aio_write(aiocb*);
+        friend int aio_fsync(int, aiocb*);
+        friend int aio_error(aiocb*);
+        friend int aio_return(aiocb*);
+        friend bool register_fd(int);
+        friend void deregister_fd(int);
+
         public:
-            static std::atomic<BTreeDefault*> bts[65536];
-            static std::unique_ptr<Controller> ctrl;
+            int             aio_fildes = 0;
+            off_t           aio_offset = 0;
+            volatile void*  aio_buf = nullptr;
+            size_t          aio_nbytes = 0;
+            int             aio_reqprio = 0;    // will ignore it
+            struct sigevent aio_sigevent;       // will ignore it
+            int             aio_lio_opcode;     // will ignore it
 
-            int             aio_fildes;
-            off_t           aio_offset;
-            volatile void  *aio_buf;
-            size_t          aio_nbytes;
-            int             aio_reqprio;    // will ignore it
-            struct sigevent aio_sigevent;   // will ignore it
-            int             aio_lio_opcode; // will ignore it
+            explicit aiocb(int fildes = 0, off_t offset = 0, volatile void* buf = nullptr, size_t nbytes = 0) : aio_fildes(fildes), aio_offset(offset), aio_buf(buf), aio_nbytes(nbytes) {}
 
-            aiocb(int fd = 0, off_t offset = 0, volatile void *buf = nullptr, size_t size = 0) : aio_fildes(fd), aio_offset(offset), aio_buf(buf), aio_nbytes(size) {}
+            void read()
+            {
+                io.reset(bts[aio_fildes].load(std::memory_order_consume)->readsome(*ctrl, aio_offset, aio_nbytes, (char *)aio_buf));
+            }
 
-            void read() { io.reset(bts[aio_fildes].load(std::memory_order_relaxed)->readsome(*ctrl, aio_offset, aio_nbytes, (char *)aio_buf)); }
+            void write()
+            {
+                io.reset(bts[aio_fildes].load(std::memory_order_consume)->write(*ctrl, aio_offset, aio_nbytes, (const char *)aio_buf));
+            }
 
-            void write() { io.reset(bts[aio_fildes].load(std::memory_order_relaxed)->write(*ctrl, aio_offset, aio_nbytes, (const char *)aio_buf)); }
-
-            void fsync() { io.reset(bts[aio_fildes].load(std::memory_order_relaxed)->sync(*ctrl)); }
+            void fsync()
+            {
+                io.reset(bts[aio_fildes].load(std::memory_order_consume)->sync(*ctrl));
+            }
 
             int status(); 
 
         private:
             std::unique_ptr<IOCtrl> io;
-    };
 
-    void aio_init();
-    int aio_read(aiocb *aiocbp);
-    int aio_write(aiocb *aiocbp);
-    int aio_fsync(int op, aiocb *aiocbp);
-    int aio_error(aiocb *aiocbp);
-    int aio_return(aiocb *aiocbp);
-    bool register_fd(int fd);
-    void deregister_fd(int fd);
+            static std::array<std::atomic<BTreeDefault*>, 65536> bts;
+            static std::unique_ptr<Controller> ctrl;
+    };
 }
