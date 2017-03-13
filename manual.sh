e16149f2af646cc5db7cb00a2f80a28f0ebcdfb8
Tongliang Liao <xkszltl@gmail.com>
2017-03-13 06:32:54 -0500

    Reduce size of 1M-overlay.

--- manual.sh
+++ manual.sh
@@ -16,7 +16,7 @@ for i in `ls log/*.log`; do echo $i; head -n11 $i | tail -n+2; grep iops $i | se
 make test_mt TEST_ARGS='30 16 16 10 6 8' 2>&1 | tee log/64K-Discrete.log
 for i in `ls log/*.log`; do echo $i; head -n11 $i | tail -n+2; grep iops $i | sed 's/:.*, /:#/' | sed 's/#\([0-9][0-9]\.\)/# \1/' | column -s\# -t; echo; echo; done
 
-make test_mt TEST_ARGS='26 20 20 12 5 7' 2>&1 | tee log/1M-Overlay.log
+make test_mt TEST_ARGS='26 20 20 10 5 7' 2>&1 | tee log/1M-Overlay.log
 for i in `ls log/*.log`; do echo $i; head -n11 $i | tail -n+2; grep iops $i | sed 's/:.*, /:#/' | sed 's/#\([0-9][0-9]\.\)/# \1/' | column -s\# -t; echo; echo; done
 
 make test_mt TEST_ARGS='31 20 20 9 4 6' 2>&1 | tee log/1M-Discrete.log
