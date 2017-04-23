c6404b59e5f0ae2e575573232176d61438de8869
Tongliang Liao <xkszltl@gmail.com>
2017-04-23 07:19:37 -0500

    Trivial Test

--- manual.sh
+++ manual.sh
@@ -13,7 +13,7 @@ fi
 make test_mt TEST_ARGS='24 4 4 15 8 10' 2>&1 | tee log/4K-Overlay.log
 for i in `ls log/*.log`; do echo $i; head -n11 $i | tail -n+2; grep iops $i | sed 's/:.*, /:#/' | sed 's/#\([0-9][0-9]\.\)/# \1/' | column -s\# -t; echo; echo; done
 
-make test_mt TEST_ARGS='30 4 4 12 8 10' 2>&1 | tee log/4K-Discrete.log
+make test_mt TEST_ARGS='30 4 4 10 7 9' 2>&1 | tee log/4K-Discrete.log
 for i in `ls log/*.log`; do echo $i; head -n11 $i | tail -n+2; grep iops $i | sed 's/:.*, /:#/' | sed 's/#\([0-9][0-9]\.\)/# \1/' | column -s\# -t; echo; echo; done
 
 make test_mt TEST_ARGS='26 64 64 14 8 10' 2>&1 | tee log/64K-Overlay.log
@@ -25,7 +25,7 @@ for i in `ls log/*.log`; do echo $i; head -n11 $i | tail -n+2; grep iops $i | se
 make test_mt TEST_ARGS='26 1024 1024 10 5 7' 2>&1 | tee log/1M-Overlay.log
 for i in `ls log/*.log`; do echo $i; head -n11 $i | tail -n+2; grep iops $i | sed 's/:.*, /:#/' | sed 's/#\([0-9][0-9]\.\)/# \1/' | column -s\# -t; echo; echo; done
 
-make test_mt TEST_ARGS='31 1024 1024 9 4 6' 2>&1 | tee log/1M-Discrete.log
+make test_mt TEST_ARGS='31 1024 1024 8 4 6' 2>&1 | tee log/1M-Discrete.log
 for i in `ls log/*.log`; do echo $i; head -n11 $i | tail -n+2; grep iops $i | sed 's/:.*, /:#/' | sed 's/#\([0-9][0-9]\.\)/# \1/' | column -s\# -t; echo; echo; done
 
 /mnt/ssd/flyysm/unlock_server.sh hao_tongliang
